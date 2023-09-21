7d2c918cbdb94e140fcb20cf851eae28834b22b7
jiangweidong <weidong.jiang@fit2cloud.com>
2023-09-21 15:03:03 +0800

    feat: 外挂脚本，支持修改操作系统的network

---
+++ main.py
@@ -0,0 +1,203 @@
+"""
+脚本提供的功能(方便运行，所有逻辑均在一个脚本中)
+1、提供健康检测接口，Linux上crontab定时任务探活使用
+2、提供JumpServer容器健康检测服务，超过检测失败次数后，执行命令jmsctl restart重启JumpServer
+3、从JumpServer中获取ip、gateway、subnet mask进行本机设置，并重启docker
+"""
+
+import os
+import logging
+import subprocess
+import shelve
+import time
+import threading
+
+from datetime import datetime
+
+from fastapi import FastAPI, APIRouter
+
+
+# ---------- 常量区域 ----------
+UOS = 'UOS'
+CENTOS = 'CentOS'
+CURRENT_OS = os.environ.get('TASK_OS', CENTOS)
+INTERVAL = os.environ.get('TASK_INTERVAL', 10)
+# ---------- 常量区域 ----------
+
+
+# ---------- 工具类 ----------
+class Tool(object):
+    def __init__(self):
+        # _thread_pool = {线程名称: {'last_time': '上次执行时间', 'count': 执行次数}}
+        self._thread_pool = {}
+        self._init_task()
+
+    def _period_check_net_config(self, task_name):
+        keys = {
+            'ip': 'JUMPSERVER_IP', 'gateway': 'JUMPSERVER_GATEWAY',
+            'subnet_mask': 'JUMPSERVER_SUBNET_MASK'
+        }
+        volume_dir = os.environ.get("VOLUME_DIR")
+        db_path = os.path.join(volume_dir, 'core', 'data', 'net_config')
+        # db_path = os.path.join(volume_dir, 'data', 'net_config')
+
+        while True:
+            db = shelve.open(db_path)
+            result = {}
+            for k, v in keys.items():
+                if p := db.pop(v):
+                    result[k] = p
+            db.close()
+            self._thread_pool[task_name]['count'] += 1
+            self._thread_pool[task_name]['last_time'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            self._thread_pool[task_name]['timestamp'] = int(time.time())
+
+            # 执行核心任务
+            if len(keys) == len(result):
+                t = threading.Thread(target=self.modify_network, args=(CURRENT_OS,), kwargs=result)
+                t.start()
+            time.sleep(INTERVAL)
+
+    def _init_task(self):
+        task_name = '周期检查JumpServer设置的网络配置'
+        thread = threading.Thread(
+            target=self._period_check_net_config, args=(task_name,),
+            name=task_name
+        )
+        self._thread_pool[task_name] = {'count': 0}
+        thread.setDaemon(True)
+        thread.start()
+
+    def get_tasks_info(self):
+        tasks, status = self._thread_pool, 'ok'
+        task_message = ''
+        for name, other in tasks.items():
+            task_message += f"任务[{name}]: 一共执行了{other['count']}次，上次执行时间是: {other['last_time']}; "
+            diff = time.time() - other.get('timestamp', 0)
+            if diff > INTERVAL * 3:
+                status = 'failed'
+        content = f"一共有{len(tasks)}个任务; {task_message}"
+        return content, status
+
+    @staticmethod
+    def get_logger():
+        log = logging.getLogger('ascarid')
+        handler = logging.FileHandler('app.log')
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        log.setLevel(logging.ERROR)
+        log.addHandler(handler)
+        return log
+
+    @staticmethod
+    def check_jms_core_status():
+        command = "docker inspect --format='{{.State.Health.Status}}' $(docker ps -aqf 'name=jms_core')"
+        try:
+            result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE)
+            health_status = result.stdout.decode().strip().lower() == 'healthy'
+            logger.debug(f'Check core status result: {health_status}')
+        except Exception as err:
+            health_status = False
+            logger.error(f'Check core status failed: {err}')
+        return health_status
+
+    @staticmethod
+    def restart_jms():
+        command = 'jmsctl restart'
+        subprocess.run(command, shell=True, check=True)
+        logger.info('The jmsctl restart command is successfully delivered')
+
+    @staticmethod
+    def restart_docker():
+        command = 'systemctl restart docker'
+        subprocess.run(command, shell=True, check=True)
+        logger.info('The docker restart command is successfully delivered')
+
+    @staticmethod
+    def __modify_network_centos(**kwargs):
+        ip = kwargs.get('ip')
+        gateway = kwargs.get('gateway')
+        subnet_mask = kwargs.get('subnet_mask')
+
+        network_card = ''
+        command = "ip route | grep default | awk -F '[ \t*]' '{{print $5}}'"
+        logger.debug(f'Centos command: {command}')
+        result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE)
+
+        routers = set(result.stdout.decode().splitlines())
+        if len(routers) < 1:
+            logger.warning('ip route 未查询到网卡名称')
+
+        path = "/etc/sysconfig/network-scripts/ifcfg-" + network_card
+        file_handler = open(path, "r")
+        network_content = file_handler.read()
+        file_handler.close()
+        conte = "IPADDR=%s\nGATEWAY=%s\nPREFIX=%s\n" % (ip, gateway, subnet_mask)
+        num = network_content.find("IPADDR")
+        if num != -1:
+            network_content = network_content[:num] + conte
+            file_handler = open(path, "w")
+            file_handler.write(network_content)
+            file_handler.close()
+
+        result = os.system('service network restart')
+        logger.info('The network restart command is successfully delivered')
+        return result == 0
+
+    def modify_network(self, os_type=CENTOS, **kwargs):
+        failed = False
+        # kwargs 目前传入的参数有ip, subnet_mask, gateway
+        os_actions = {
+            UOS: [
+                'nmcli con mod eno1  ipv4.addresses {ip}',
+                'nmcli con mod eno1  ipv4.addresses {ip}/{subnet_mask}',
+                'nmcli con mod eno1  ipv4.gateway {gateway}',
+                'nmcli con up eno1'
+            ],
+            CENTOS: self.__modify_network_centos
+        }
+        action = os_actions.get(str(os_type).lower())
+        if not action:
+            logger.warning(f'此操作系统({os_type})下未匹配到对应命令.')
+            return
+
+        if isinstance(action, list):
+            for command in action:
+                try:
+                    full_command = command.format(**kwargs)
+                except KeyError:
+                    continue
+
+                try:
+                    subprocess.run(full_command, shell=True, check=True)
+                    logger.info(f'Command [{full_command}] executed successfully')
+                except Exception as err:
+                    failed = True
+                    logger.error(f'Command [{full_command}], executed failed: {err}')
+        elif callable(action):
+            action(**kwargs)
+        if not failed:
+            self.restart_docker()
+# ---------- 工具类 ----------
+
+
+tool = Tool()
+app = FastAPI()
+api_router = APIRouter(prefix='/api')
+logger = tool.get_logger()
+
+
+# ---------- 路由 ----------
+@app.get('/')
+async def index():
+    return {'message': 'hello world'}
+
+
+@api_router.get('/health')
+async def health():
+    info, status = tool.get_tasks_info()
+    return {'status': status, 'result': info}
+# ---------- 路由 ----------
+
+
+app.include_router(api_router)
---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 这里需要把堡垒机的VOLUME_DIR配置写在这里
+export VOLUME_DIR=/data/jumpserver
+# 这里配置一下这个机器的操作系统, 只能是 UOS 和 CentOS
+export TASK_OS=CentOS
+# 这里配置一下这个任务执行的超时时间，默认30秒，
+# 健康检查接口检测机制: 如果当前和上次任务执行时间差值超过此值3倍，服务将标记为不健康
+export TASK_INTERVAL=10
+#export VOLUME_DIR=/Users/jiangweidong/resources/dazhong_v2.19/jumpserver
+# DEV
+#uvicorn main:app --reload
+uvicorn main:app
